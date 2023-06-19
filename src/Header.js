5563fba2c70ad44c06d680bc8440182c6c88d1b8
wubu <wubeabera123@gmail.com>
2023-06-19 18:30:15 +0300

    button

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,11 @@
 import React from "react";
 function Header(){
     return(
-     <h1>Hello World</h1>
+        <div>
+          <h1>Hello World</h1>
+          <button type="button" class="btn btn-primary">Primary</button>
+        </div>  
+     
     )
 }
 export default Header;
