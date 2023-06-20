41f5030b936be6eea609e4a96e6dec5a7b603d57
wubu <wubeabera123@gmail.com>
2023-06-20 17:49:14 +0300

    nav

--- src/Header.js
+++ src/Header.js
@@ -2,12 +2,12 @@ import React from "react";
 import "./index.css"
 function Header(){
     return(
-     <nav class="navbar navbar-expand-lg bg-body-tertiary">
+     <nav class="navbar navbar-expand-lg bg-body-tertiary mx-auto">
        <div class="container-fluid">
           <a class="navbar-brand " className="title" href="#">learnable</a>
        </div>  
-       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ms-auto">
+       <div class="container mx-auto"  id="navbarSupportedContent">
+          <ul className="navbar-nav ">
               <li class="nav-item">
                 <a class="nav-link active" href="#">Home</a>
               </li>
@@ -21,8 +21,12 @@ function Header(){
                 <a class="nav-link" href="#">About</a>
               </li>
           </ul>
+          <form class="d-flex" role="search"> 
+             <input class="form-control me-2" type="search" placeholder="Search"></input>
+             <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
        </div>
-     </nav>
+      </nav>
     )
 }
 export default Header;
