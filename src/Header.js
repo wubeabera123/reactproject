0f7934c9403cb9f7eef3244d5e6f986fdece2642
wubu <wubeabera123@gmail.com>
2023-06-20 10:41:45 +0300

    nav

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,28 @@
 import React from "react";
+import "./index.css"
 function Header(){
     return(
-        <div>
-          <h1>Hello World</h1>
-          <button type="button" class="btn btn-primary">Primary</button>
-        </div>  
-     
+     <nav class="navbar navbar-expand-lg bg-body-tertiary">
+       <div class="container-fluid">
+          <a class="navbar-brand " className="title" href="#">learnable</a>
+       </div>  
+       <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto">
+              <li class="nav-item">
+                <a class="nav-link active" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Categories</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Mentors</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+              </li>
+          </ul>
+       </div>
+     </nav>
     )
 }
 export default Header;
